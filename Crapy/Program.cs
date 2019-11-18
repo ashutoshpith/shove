c16f114044fbac79dee2e67708653fb213a6dd0e
ashutoshpith <amxdp18@gmail.com>
2019-11-19 00:40:42 +0530

    json file same for result value

--- Crapy/Program.cs
+++ Crapy/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Text;
+using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -20,32 +21,34 @@ namespace Crapy
                
                 driver.Navigate().GoToUrl("https://www.cellspare.com/other/realme-spare-parts/realme-xt-spare-parts/realme-xt-lcd-screen-with-digitizer-module-black");
 
-               // driver.Manage().Window.Size = new Size(2, 2);
-                //var userNameField = driver.FindElementById("usr");
-                //var userPasswordField = driver.FindElementById("pwd");
-                //var loginButton = driver.FindElementByXPath("//input[@value='Login']");
-
-
-                //userNameField.SendKeys("admin");
-                //userPasswordField.SendKeys("12345");
-
-                //// and click the login button
-                //loginButton.Click();
 
                 // Extract the text and save it into result.txt
                 var result = driver.FindElementByXPath("//div[@class='location-bar']/h1").Text;
                 var res = driver.FindElementByXPath("//div[@class='price']/div[@class='price-regular']").Text;
                 // var result = driver.FindElement(By.XPath("//div/a")).ToString();
                 //IReadOnlyList<IWebElement> muchoCheese = driver.FindElements(By.CssSelector("#cheese li"));
-               
-                List<String> ll = new List<string>();
-                ll.Add(result);
-                ll.Add(res);
-                
-                foreach (String s in ll)
+                Type type = new Type
                 {
-                    File.AppendAllText("result.txt", "\n"+s+"\t");
-                }
+                    Name = result,
+                    Price = res
+
+                };
+
+                // serialize JSON to a string and then write string to a file
+                File.AppendAllText("result.json", JsonConvert.SerializeObject(type));
+
+              
+
+
+
+                //List<String> ll = new List<string>();
+                //ll.Add(result);
+                //ll.Add(res);
+
+                //foreach (String s in ll)
+                //{
+                //    File.AppendAllText("result.json", "\n"+s+"\t");
+                //}
                 driver.Quit();
                 Console.WriteLine("here is the result "+result);
               
---
+++ Crapy/Type.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crapy
+{
+    class Type
+    {
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+    }
+}
